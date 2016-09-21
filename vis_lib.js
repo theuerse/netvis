76f223c4c4fbb8cc21c64e752da08c7aab79274c
theuerse <theuers@posteo.de>
2016-09-21 10:35:21 +0200

    modfied vis_lib and networkLayout.js to have no overlap

--- vis_lib.js
+++ vis_lib.js
@@ -113,10 +113,8 @@ function changeModeOfOperation(traffic, rtlog){
   resetClientImages(clients);
 
 
-
   //TODO: Do that! (break down the old)
   mode = {traffic: traffic, rtlog: rtlog}; // update mode of operation
-
 
 
   // start the new
@@ -156,7 +154,6 @@ function changeModeOfOperation(traffic, rtlog){
   }
 
 }
-
 
 // draws given topology-data using vis.js (data from e.g. "generated_network_top.txt")
 function drawTopology(data){
@@ -328,112 +325,6 @@ function drawTopology(data){
         if(params.edges.length == 1){
           showEdgeCooltip(params.edges[0],network);
           toggleCooltipPinned(params.edges[0]);
-        }
-      });
-    }
-
-    // Draws a legend containing met-information about the networkLayout (/ Topology)
-    function drawLegend(network,options,numberOfNodes,servers,groups,bitrateBounds){
-      $('#legendContainer').append('<ul id="legendList" class="list-group">' +
-      '<li id="legendGraph" class="noPadding list-group-item"></li>' +
-      '</ul>');
-
-      var nodes = new vis.DataSet();
-      var edges = new vis.DataSet();
-
-      var container = document.getElementById('legendGraph');
-      // coordinates originating in midpoint
-      var x = container.clientWidth / 2;
-      var y = container.clientHeight / 2;
-      var step = 80;
-
-      options.height = '265px'; // limit height, make room for additional information
-      options.interaction = {zoomView: false, selectable: false, dragView: false};
-      options.physics = {enabled: false};
-
-      var serverCount = servers.length;
-      var clientCount = 0;
-      for(var key in groups){
-        clientCount += groups[key].length;
-      }
-
-      nodes.add({id: 1, x: x, y: y, label: 'Router' + ' (' + (numberOfNodes - (serverCount + clientCount)) + ')',
-      shape: "image", image: images.router[0], fixed: true, shadow: true, physics:false});
-      nodes.add({id: 2, x: x, y: y + step, label: 'Router + Server' + ' (' + serverCount + ')',
-      shape: "image", image: images.server[0], fixed: true, shadow: true, physics:false});
-      nodes.add({id: 3, x: x, y: y + 2 * step, label: 'Router + Client' + ' (' + clientCount + ')',
-      shape: "image", image: images.client[0], fixed: true, shadow: true,  physics:false});
-
-      var data = {nodes: nodes,edges: edges};
-      // draw legend
-      var legend = new vis.Network(container, data, options);
-
-      // add additional information
-      // min-/ max-Bitrate
-      // display of min-/max bitrate of disabled for now
-      /*
-      $('#legendList').append('<li class="list-group-item"><b>min bitrate: </b>' + bitrateBounds[0] + '[kbps]</li>');
-      $('#legendList').append('<li class="list-group-item"><b>max bitrate: </b>' + bitrateBounds[1] + '[kbps]</li>');
-      */
-
-      // add group information
-      var groupsInfo = "";
-
-      // for every server (group-leader)
-      servers.forEach(function(entry) {
-        var members = $.merge([entry],groups[entry]).sort(function(a,b){return a - b;}); // sort as numbers
-        groupsInfo += '<h3 id="grpHeader' + entry +'" style="color: ' + colors[$.inArray(entry,servers)] +'">Group ' + ($.inArray(entry,servers)+1) + '</h3>' +
-        '<div>' +
-        '<p>' + members + '</p>' +
-        '</div>';
-      });
-
-      $("#legendList").append('<li class="noPadding list-group-item"><div id="grpAccordion">' + groupsInfo + '</div></li>');
-      $("#grpAccordion").accordion({active: false, collapsible: true});
-
-      servers.forEach(function(entry) {
-        $('#grpHeader' + entry).bind('click', function (e) {
-          if($('#grpHeader' + entry).attr("aria-expanded") === "true"){
-            network.selectNodes($.merge([entry],groups[entry]));
-          }else {
-            network.selectNodes([]);
-          }
-
-          // highlight selected group-nodes
-          highlightSelectedNodes(network);
-        });
-      });
-
-
-      // get params
-      var seed = getUrlVar("seed");
-
-      // add buttons for toggling traffic / rtLog - watching on or off
-      $('#legendList').append('<li class="list-group-item"><div id="btnGrp">'+
-      '<label for="trafficToggle">watch traffic</label><input type="checkbox" id="trafficToggle"/>' +
-      '<label for="rtLogToggle">read rtLogs</label><input type="checkbox" id="rtLogToggle" />' +
-      '</div></li>');
-
-      $('#btnGrp').buttonset();
-
-      $('#trafficToggle').bind('change', function(){
-        if($(this).is(':checked')){
-          $(this).button('option', 'label', "ignore traffic");
-          changeModeOfOperation(true,mode.rtlog);
-        }else{
-          $(this).button('option', 'label', "watch traffic");
-          changeModeOfOperation(false,mode.rtlog);
-        }
-      });
-
-      // add toggle-button for traffic      $("#rtLogToggle").button();
-      $('#rtLogToggle').bind('change', function(){
-        if($(this).is(':checked')){
-          $(this).button('option', 'label', "ignore rtLogs");
-          changeModeOfOperation(mode.traffic,true);
-        }else{
-          $(this).button('option', 'label', "read rtLogs");
-          changeModeOfOperation(mode.traffic,false);
         }
       });
     }
